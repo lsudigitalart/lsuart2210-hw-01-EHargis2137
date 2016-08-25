b629744acf1e45c07dfc6f9622d5d3cc18572453
Emily Hargis <emilyhargis@3c-15-c2-d9-1d-32.wlan.lsu.edu>
2016-08-25 11:37:11 -0500

    done black and white

---
+++ sketch_bw.js
@@ -0,0 +1,62 @@
+function setup() {
+  createCanvas(500,500);
+  fill('black');
+  ellipse(125, 75, 60, 60);
+  ellipse(75, 125, 60, 60);
+  ellipse(75, 75, 60, 60);
+  ellipse(125, 125, 60, 60);
+  fill('white');
+  ellipse(100, 150, 40, 80);
+  ellipse(100, 50, 40, 80);
+  ellipse(150, 100, 80, 40);
+  ellipse(50, 100, 80, 40);
+  ellipse(100, 100, 20);
+
+  fill('white');
+  ellipse(225, 175, 60, 60);
+  ellipse(175, 225, 60, 60);
+  ellipse(175, 175, 60, 60);
+  ellipse(225, 225, 60, 60);
+  fill('black');
+  ellipse(200, 250, 40, 80);
+  ellipse(200, 150, 40, 80);
+  ellipse(250, 200, 80, 40);
+  ellipse(150, 200, 80, 40);
+  ellipse(200, 200, 20);
+
+  fill('black');
+  ellipse(325, 275, 60, 60);
+  ellipse(275, 325, 60, 60);
+  ellipse(275, 275, 60, 60);
+  ellipse(325, 325, 60, 60);
+  fill ('white');
+  ellipse(300, 350, 40, 80);
+  ellipse(300, 250, 40, 80);
+  ellipse(350, 300, 80, 40);
+  ellipse(250, 300, 80, 40);
+  ellipse(300, 300, 20);
+
+  fill('black');
+  ellipse(325, 75, 60, 60);
+  ellipse(275, 125, 60, 60);
+  ellipse(275, 75, 60, 60);
+  ellipse(325, 125, 60, 60);
+  fill('white');
+  ellipse(300, 50, 40, 80);
+  ellipse(300, 150, 40, 80);
+  ellipse(350, 100, 80, 40);
+  ellipse(250, 100, 80, 40);
+  ellipse(300, 100, 20);
+
+  fill('black');
+  ellipse(125, 275, 60, 60);
+  ellipse(75, 325, 60, 60);
+  ellipse(75, 275, 60, 60);
+  ellipse(125, 325, 60, 60);
+  fill('white');
+  ellipse(100, 350, 40, 80);
+  ellipse(100, 250, 40, 80);
+  ellipse(150, 300, 80, 40);
+  ellipse(50, 300, 80, 40);
+  ellipse(100, 300, 20);
+}
